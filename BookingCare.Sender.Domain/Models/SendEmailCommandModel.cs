3efacc2f506e62c16ffa90bd5051365a013f4914
Nguyễn Minh Quang <nguyenminhquangg03012004@gmail.com>
2026-03-15 23:18:56 +0700

    Delete BookingCare.Sender.Domain directory

--- BookingCare.Sender.Domain/Models/SendEmailCommandModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookingCare.Sender.Domain.Models
-{
-    public class SendEmailCommandModel
-    {
-        public IList<string> ToEmails { get; set; } = new List<string>();
-        public string? Subject { get; set; }
-        public string? Content { get; set; }
-    }
-}
